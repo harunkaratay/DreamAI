877e81177f6fd3fbd2c987bbb2c1e759f56b6d78
harunkaratay <harunkaratayy@outlook.com>
2026-04-29 16:35:16 +0300

    7. hafta için gerekli linkler eklendi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,7 @@
                             <span class="text-gray-600 dark:text-gray-300 font-medium text-sm sm:text-base flex-1">
                                 Bilinçaltınızın gizemli dünyasını keşfetmeye ve sanata dönüştürmeye hazır mısınız? Giriş yapıp sihire kapılabilirsiniz.
                             </span>
-                    <a href="{{-- route('dreamIndex') --}}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                    <a href="{{route('dreamIndex') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
                         Hadi Başlayalım
                     </a>
                 </li>
